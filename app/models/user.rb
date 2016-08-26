d32a3614e0eeeefde617777f880bdd3974675160
Connor Paull <connor.paull@gmail.com>
2016-08-26 12:20:32 -0700

    minor bug fixes

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,5 @@ class User < ActiveRecord::Base
 	has_many :joined_orgs, through: :memberships, source: :organization
 	EMAIL_REGEX = /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]+)\z/i
   	validates :first_name, :last_name, :email, :password, presence: true, :on => :create
- 	validates :email, presence: true, uniqueness: { case_sensitive: false }, format: { with: EMAIL_REGEX }
+ 	validates :email, uniqueness: { case_sensitive: false }, format: { with: EMAIL_REGEX }
 end
